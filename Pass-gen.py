3a2d4f60ba689853e33417b1f4dea18b2ac9a972
SIDDHARTH <siddharthbendigeri238@gmail.com>
2024-10-23 14:46:58 +0530

    Codsoft task

--- Pass-gen.py
+++ Pass-gen.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter import messagebox
@@ -32,15 +31,15 @@ all_no = {"1" : "1", "2" : "2", "3" : "3", "4" : "4","5" : "5", "6" : "6", "7" :
 Title = Label(root, text="Password Generator", bg="beige", fg="black", font=("futura", 15, "bold"))
 Title.pack(anchor="center", pady="20px")
 
-length = Label(root, text="Select the Length of Your Password :- ", fg="darkgreen", bg="beige", font=("ubuntu", 12))
+length = Label(root, text="Select the Length of Your Password :- ", fg="black", bg="beige", font=("ubuntu", 12))
 length.place(x="20px", y="70px")
 
 def on_enter(e):
-    generate_btn['bg'] = "grey"
+    generate_btn['bg'] = "green"
     generate_btn['fg'] = "white"
 
 def on_leave(e):
-    generate_btn['bg'] = "orange"
+    generate_btn['bg'] = "blue"
     generate_btn['fg'] = "black"   
 
 solidboss = IntVar()
@@ -49,12 +48,12 @@ Selector['values'] = [l for l in all_no.keys()]
 Selector.current(7)
 Selector.place(x="240px", y="72px")
 
-generate_btn = Button(root, text="Generate Password", bg="orange", fg="black", font=("ubuntu", 15), cursor="hand2", command=password_generate)
+generate_btn = Button(root, text="Generate Password", bg="blue", fg="black", font=("ubuntu", 15), cursor="hand2", command=password_generate)
 generate_btn.bind("<Enter>", on_enter)
 generate_btn.bind("<Leave>", on_leave)
 generate_btn.pack(anchor="center", pady="50px")
 
-result_lable = Label(root, text="Generated Password Here :- ", bg="beige", fg="darkgreen", font=("ubuntu", 12))
+result_lable = Label(root, text="Generated Password Here :- ", bg="beige", fg="black", font=("ubuntu", 12))
 result_lable.place(x="20px", y="160px")
 
 password = StringVar()
